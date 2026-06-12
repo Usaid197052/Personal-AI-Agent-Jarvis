46ef189f434efd034872ea2c9019c5a2007b982f
Usaid Ahmed <usaid33376@gmail.com>
2026-06-12 12:34:25 +0500

    Update tool_registry.py

--- tools/tool_registry.py
+++ tools/tool_registry.py
@@ -6,10 +6,25 @@ from tools.app_tools import (
     open_powershell
 )
 
+from tools.file_tools import (
+    create_file,
+    read_file,
+    list_files
+)
+
+
 TOOLS = {
+
+    # Application Tools
     "open_visual_studio": open_visual_studio,
     "open_notepad": open_notepad,
     "open_calculator": open_calculator,
     "open_cmd": open_cmd,
-    "open_powershell": open_powershell
+    "open_powershell": open_powershell,
+
+    # File Tools
+    "create_file": create_file,
+    "read_file": read_file,
+    "list_files": list_files
+
 }
