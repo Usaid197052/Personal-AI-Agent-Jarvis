d13ef594845ccdd27bbbe7b6564800ed9c6c6281
Usaid Ahmed <usaid33376@gmail.com>
2026-06-12 12:31:01 +0500

    Update action_executor.py

--- executor/action_executor.py
+++ executor/action_executor.py
@@ -1,9 +1,15 @@
 from tools.tool_registry import TOOLS
 
 
-def execute_action(action_name):
+def execute_action(tool_name, arguments):
 
-    if action_name not in TOOLS:
-        return f"Tool '{action_name}' not found."
+    if tool_name not in TOOLS:
+        return f"Tool '{tool_name}' not found."
 
-    return TOOLS[action_name]()
+    tool_function = TOOLS[tool_name]
+
+    try:
+        return tool_function(**arguments)
+
+    except Exception as e:
+        return f"Execution Error: {e}"
