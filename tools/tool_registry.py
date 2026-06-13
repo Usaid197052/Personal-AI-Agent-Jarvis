ae52c864c72f622fde3f87808bc9d263b470aa10
Usaid Ahmed <usaid33376@gmail.com>
2026-06-13 16:18:54 +0500

    updated files, system and terminal tools registry

--- tools/tool_registry.py
+++ tools/tool_registry.py
@@ -9,7 +9,23 @@ from tools.app_tools import (
 from tools.file_tools import (
     create_file,
     read_file,
-    list_files
+    list_files,
+    delete_file,
+    rename_file,
+    move_file,
+    copy_file
+)
+
+from tools.terminal_tools import (
+    run_python_script,
+    run_cmd_command,
+    run_powershell_command
+)
+
+from tools.system_tools import (
+    shutdown_pc,
+    restart_pc,
+    sleep_pc
 )
 
 
@@ -25,6 +41,19 @@ TOOLS = {
     # File Tools
     "create_file": create_file,
     "read_file": read_file,
-    "list_files": list_files
+    "list_files": list_files,
+    "delete_file": delete_file,
+    "rename_file": rename_file,
+    "move_file": move_file,
+    "copy_file": copy_file,
 
+    # Terminal Tools
+    "run_python_script": run_python_script,
+    "run_cmd_command": run_cmd_command,
+    "run_powershell_command": run_powershell_command,
+
+    # System Tools
+    "shutdown_pc": shutdown_pc,
+    "restart_pc": restart_pc,
+    "sleep_pc": sleep_pc
 }
