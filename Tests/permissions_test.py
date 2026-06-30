deb26d2134fb740f2216c98c00c8d1b56b266bc4
Usaid Ahmed <usaid33376@gmail.com>
2026-06-30 15:22:27 +0500

    Revert "Create permissions_test.py"
    This reverts commit 4ebeca12d67ac0e0a5a9c2a59af37ad6e1534be7.

--- Tests/permissions_test.py
+++
@@ -1,8 +0,0 @@
-from permissions.permissions import requires_confirmation
-
-
-print("shutdown_pc:",
-      requires_confirmation("shutdown_pc"))
-
-print("open_notepad:",
-      requires_confirmation("open_notepad"))
